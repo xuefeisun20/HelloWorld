d194c09757c30035e77ebbf682cbfe751896fd4e
sun Xuefei <xuefei_sun@outlook.com>
2020-10-27 22:08:05 +0800

    test

--- HelloWorld/Startup.cs
+++ HelloWorld/Startup.cs
@@ -23,6 +23,7 @@ namespace HelloWorld
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // test 001
             services.AddControllersWithViews();
         }
 
