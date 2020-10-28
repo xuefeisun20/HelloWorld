3416442933cdf3549c6b2f4c452958391e03d592
sun Xuefei <xuefei_sun@outlook.com>
2020-10-28 19:01:36 +0800

    tesdt

---
+++ HelloWorld/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Test
+    {
+        public void AddMonth(int num1,int num2)
+        {
+            int num = num1 + num2;
+        }
+    }
+}
