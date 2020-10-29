cdd6a98cd3feece2bea78c65fa183fc1b692b1eb
sun Xuefei <xuefei_sun@outlook.com>
2020-10-29 21:57:40 +0800

    test

---
+++ HelloWorld/bin/netcoreapp3.1/test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class test
+    {
+        public void ConfigureServices(int num)
+        {
+            int nmber = num;
+        }
+    }
+}
